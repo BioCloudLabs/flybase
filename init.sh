e01f237978b9be232f86651714cc817bf8373e57
albertmartinmoreno <160793560+albertmartinmoreno@users.noreply.github.com>
2024-05-14 16:39:33 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-
+mkdir fasta blastdb_custom
+chmod -R 777 fasta/ blastdb_custom/
+apt install python3-pip docker.io -y 
+pip install docker
+docker pull ncbi/blast
 (crontab -l ; echo "00 00 * * * /usr/bin/python3 update_blastdb.py") | crontab -
